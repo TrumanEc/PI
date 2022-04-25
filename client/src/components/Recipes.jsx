0fc90b3ce8c3a0ad91bd603c6e8dc9104fff0a9f
Darwin Esteban Cifuentes Garay <darwinestebancifuentesgaray@MacBook-Pro-de-Darwin.local>
2022-04-25 00:05:28 -0500

    pagination has been created

--- client/src/components/Recipes.jsx
+++ client/src/components/Recipes.jsx
@@ -1,29 +1,38 @@
-import React, { useEffect} from "react";
+import React, { useEffect, useState} from "react";
 import { useSelector}  from 'react-redux'
 //import { getRecipes } from "../actions";
 import CardRecipe from "./CardRecipe";
+import Pagination from "./Pagination";
 import { Link } from "react-router-dom";
+
 
 export default function Recipes (){
     
     const allRecipes = useSelector(state => state.recipes)
 
-    //const [recipes, setRecipes] = useState(props.recipes);
-    
-    // const dispatch = useDispatch()
+    const [page, setPage] = useState (1);
+    const [perPage, setPerPage] = useState (9);
+
+    const max = allRecipes.length / perPage;
 
     useEffect(()=>{
         console.log('Renderin Recipes')
     },[])
-
     
+    useEffect(()=>{
+        setPage(1);
+    },[allRecipes])
 
     return (
         <div className="Recipes" >
         <Link to='/createRecipe'> Create recipe </Link>
         <div>Recipes</div>
         {   
-            (allRecipes.length > 0) ? allRecipes.map(r => {
+            (allRecipes.length > 0) 
+                ? allRecipes.slice(
+                    (page - 1) * perPage,
+                    (page - 1) * perPage + perPage
+                ).map(r => {
                 return <CardRecipe
                             key={r.id}
                             id={r.id}
@@ -35,6 +44,7 @@ export default function Recipes (){
                 
             }) : <h2>{allRecipes.msg}</h2>
         }
+        <Pagination  page={page} setPage={setPage} max={max}/>
         </div>
     )
 }
